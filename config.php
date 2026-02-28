f0556d953a88ff7a9796a3ef45092ef17fd910ee
pcpro27 <moulintiago05092012@gmail.com>
2026-02-28 20:47:20 +0100

    Ajout du fichier de configuration config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Configuration de la base de données
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'text_editor');
+
+// Chemins
+define('DOCUMENTS_DIR', __DIR__ . '/documents/');
+?>
